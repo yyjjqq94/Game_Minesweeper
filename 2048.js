726130bb8539121516332d1ff7633b25f3c43f1d
yyjjqq94 <727834249@qq.com>
2017-12-08 00:14:36 +0800

    2048
    2048

--- 2048.js
+++ 2048.js
@@ -15,6 +15,8 @@ var Game = function () {
     this.m = [];
     this.empty = [];
     _this = this;
+    this.data = [];
+    this.gameGo =true;
 
     this.start = function () {
         $('#container').html('');
@@ -37,7 +39,7 @@ var Game = function () {
                     break;
             }
         })
-    }
+    };
 
     this.createBox = function () {
         for(var i = 0;i < this.x;i++){
@@ -49,51 +51,123 @@ var Game = function () {
             }
             $('#container').append("<br>");
         }
-    }
+    };
 
     this.createRandom = function () {
         //random
-        var random = Math.random() < 0.5 ? 2 : 4;
-        //find empty box
-        for(var i = 0;i < this.x;i++){
-            for(var j = 0;j < this.y;j++){
-                if(this.m[i][j] == 0){
-                    this.empty.push([i,j]);
+        if(_this.gameGo){
+            var random = Math.random() < 0.5 ? 2 : 4;
+            //find empty box
+            for(var i = 0;i < this.x;i++){
+                for(var j = 0;j < this.y;j++){
+                    if(this.m[i][j] == 0){
+                        this.empty.push([i,j]);
+                    }
                 }
             }
-        }
 
-        if(this.empty.length > 0){
-            var rx = Math.floor(Math.random() *this.x),
-                ry = Math.floor(Math.random() *this.y),
-                rb = $('.box[x=' + rx + '][y=' + ry + ']');
-            rb.text(random);
+            if(this.empty.length > 0){
+                var r = Math.floor(Math.random() *this.empty.length),
+                    rx = this.empty[r][0],
+                    ry = this.empty[r][1];
+                _this.m[rx][ry] = random;
+            }
+            this.empty = [];
+            this.showNumber();
         }
-    }
+    };
     
     this.moveLeft = function () {
         // alert('left');
         for(var i = 0;i < this.x;i++){
             for(var j =1;j < this.y;j++){
-                if(this.m[i][j] != 0){
+                if(_this.m[i][j] != 0){
+                    for(var k = 0;k < j;k++){
+                        if(_this.m[i][k] == 0 && _this.rowCheck(i,j,k)){
+                            _this.m[i][k] = _this.m[i][j];
+                            _this.m[i][j] = 0;
+                            continue;
+                        }else if(_this.m[i][k] == this.m[i][j] && _this.rowCheck(i,j,k)){
+                            _this.m[i][k] += _this.m[i][j];
+                            _this.m[i][j] = 0;
+                        }
+                    }
                     
                 }
             }
         }
-
-    }
+        this.showNumber();
+        this.createRandom();
+    };
     
     this.moveUp = function () {
-        alert('up');
-    }
+        // alert('up');
+        for(var i = 1;i < this.x;i++){
+            for(var j =0;j < this.y;j++){
+                if(_this.m[i][j] != 0){
+                    for(var k = 0;k < i;k++){
+                        if(_this.m[k][j] == 0 && _this.columnCheck(i,j,k)){
+                            _this.m[k][j]= _this.m[i][j];
+                            _this.m[i][j] = 0;
+                            continue;
+                        }else if(_this.m[k][j] == this.m[i][j] && _this.columnCheck(i,j,k)){
+                            _this.m[k][j] += _this.m[i][j];
+                            _this.m[i][j] = 0;
+                        }
+                    }
+
+                }
+            }
+        }
+        this.showNumber();
+        this.createRandom();
+    };
     
     this.moveRight = function () {
-        alert('right');
-    }
+        // alert('right');
+        for(var i = 0;i < this.x;i++){
+            for(var j =this.y-2;j >= 0;j--){
+                if(_this.m[i][j] != 0){
+                    for(var k = this.y-1;k > j;k--){
+                        if(_this.m[i][k] == 0 && _this.rowCheck(i,j,k)){
+                            _this.m[i][k] = _this.m[i][j];
+                            _this.m[i][j] = 0;
+                            continue;
+                        }else if(_this.m[i][k] == this.m[i][j] && _this.rowCheck(i,j,k)){
+                            _this.m[i][k] += _this.m[i][j];
+                            _this.m[i][j] = 0;
+                        }
+                    }
+
+                }
+            }
+        }
+        this.showNumber();
+        this.createRandom();
+    };
     
     this.moveDown =function () {
-        alert('down');
-    }
+        // alert('down');
+        for(var i = this.x-2;i >= 0;i--){
+            for(var j =0;j < this.y;j++){
+                if(_this.m[i][j] != 0){
+                    for(var k = this.x-1;k > i;k--){
+                        if(_this.m[k][j] == 0 && _this.columnCheck(i,j,k)){
+                            _this.m[k][j] = _this.m[i][j];
+                            _this.m[i][j] = 0;
+                            continue;
+                        }else if(_this.m[k][j] == this.m[i][j] && _this.columnCheck(i,j,k)){
+                            _this.m[k][j] += _this.m[i][j];
+                            _this.m[i][j] = 0;
+                        }
+                    }
+
+                }
+            }
+        }
+        this.showNumber();
+        this.createRandom();
+    };
     
     this.rowCheck = function (i,j,k) {
         //to left
@@ -107,15 +181,15 @@ var Game = function () {
             return true;
         }else if(k > j){
             //to right
-            k += 1;
-            for(;k < _this.y;k++){
-                if(_this.m[i][k]){
+            k -= 1;
+            for(;k > j;k--){
+                if(_this.m[i][k] != 0){
                     return false;
                 }
             }
             return true;
         }
-    }
+    };
     
     this.columnCheck = function (i,j,k) {
         //to up
@@ -128,9 +202,56 @@ var Game = function () {
             }
             return true;
         }else if(k > i){
-
+            k -=1;
+            for(;k>i;k--){
+                if(_this.m[k][j] != 0){
+                    return false;
+                }
+            }
+            return true;
         }
+    };
+
+    this.showNumber = function () {
+        for(var i =0;i<this.x;i++){
+            for(var j =0;j<this.y;j++){
+                if(this.m[i][j] != 0){
+                    $('.box[x='+ i +'][y='+ j +']').text(this.m[i][j]);
+                }else {
+                    $('.box[x='+ i +'][y='+ j +']').text('');
+                }
+            }
+        }
+
+        this.dataCheck();
+    };
+
+    this.dataCheck = function () {
+        for(var i =0;i<this.x;i++){
+            for(var j =0;j<this.y;j++){
+                this.data.push(this.m[i][j]);
+            }
+        }
+        //2048
+        if(_this.gameWin()){
+            alert('YOU WIN!!');
+            _this.gameGo = false;
+        }
+        // else if(_this.gameOver()){
+        //     alert('Try Again!');
+        //     _this.gameGo = false;
+        // }
+
+        this.data = [];
     }
+    
+    this.gameWin = function () {
+        return this.data.some(function (v) { return v == 2048 })
+    }
+
+    // this.gameOver = function () {
+    //     return this.data.every(function (v) { return v != 0 && v != 2048 })
+    // }
 
 
 }
